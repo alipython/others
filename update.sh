fe20e64c781c13a3f466ffedc8f333c2bd2f424c
alipython <ali.chengjun@gmail.com>
2017-05-31 09:35:26 +0100

    update

---
+++ update.sh
@@ -0,0 +1,15 @@
+# vim filetype=sh
+#! /bin/bash
+# **************************************
+# 
+# @Author         Noxaean
+# @Email          noxaean@gmail.com
+# @Description  
+# @Created        2017-05-31 16:34:31
+# @Last Modified  2017-05-31 16:35:17
+# 
+# **************************************
+
+git add .
+git commit -m "update"
+git push origin master
